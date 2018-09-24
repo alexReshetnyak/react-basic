35853c077147e7827a35e2a80ba33ae8b100acfa
AReshetnyak <areshetnyak@qualium-systems.com.ua>
2018-09-24 18:43:37 +0300

    was created vidly app

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,15 +2,54 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  renderTags() {
+    if (!this.state.tags.length) {
+      return <p>There are no tags</p>;
+    }
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  handleIncrement = product => {
+    console.log("Increment clicked", product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {!this.state.tags.length && "please create tag"}
+        {this.renderTags()}
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += !this.state.count ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
