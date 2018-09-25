836cffe9d03a13dffc70b94cb42e0812e1c5ef24
A Reshetnyak <alexreshetnyakmail@gmail.com>
2018-09-25 21:41:04 +0300

    modify liike component

--- vidly/src/components/common/like.jsx
+++ vidly/src/components/common/like.jsx
@@ -1,17 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Like extends Component {
-  render() {
-    return (
-      <div onClick={this.props.onLike} style={{ cursor: "pointer" }}>
-        {this.props.like ? (
-          <i className="fa fa-heart" aria-hidden="true" />
-        ) : (
-          <i className="fa fa-heart-o" aria-hidden="true" />
-        )}
-      </div>
-    );
-  }
-}
+const Like = props => {
+  return (
+    <div onClick={props.onLike} style={{ cursor: "pointer" }}>
+      {props.like ? (
+        <i className="fa fa-heart" aria-hidden="true" />
+      ) : (
+        <i className="fa fa-heart-o" aria-hidden="true" />
+      )}
+    </div>
+  );
+};
 
 export default Like;
+
+// class Like extends Component {
+//   render() {
+//     return (
+//       <div onClick={this.props.onLike} style={{ cursor: "pointer" }}>
+//         {this.props.like ? (
+//           <i className="fa fa-heart" aria-hidden="true" />
+//         ) : (
+//           <i className="fa fa-heart-o" aria-hidden="true" />
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Like;
