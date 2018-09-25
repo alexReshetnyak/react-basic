514e3594522f5a860af9ef891ef78e7a668662c1
AReshetnyak <areshetnyak@qualium-systems.com.ua>
2018-09-25 15:26:47 +0300

    was created navbar componet

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,60 +1,42 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
-  };
-
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  renderTags() {
-    if (!this.state.tags.length) {
-      return <p>There are no tags</p>;
-    }
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
-  handleIncrement = product => {
-    console.log("Increment clicked", product);
-    this.setState({ count: this.state.count + 1 });
-  };
-
   render() {
+    // * this.props.children - Html code passed to this component
     return (
       <React.Fragment>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {!this.state.tags.length && "please create tag"}
-        {this.renderTags()}
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </React.Fragment>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += !this.state.count ? "warning" : "primary";
+    classes += !this.props.counter.value ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
