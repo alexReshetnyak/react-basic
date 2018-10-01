2cfe7ab456213c4e2474d08a06442070d6b9ffd5
AReshetnyak <areshetnyak@qualium-systems.com.ua>
2018-10-01 09:42:17 +0300

    router-app: implemented nested navigation

--- router-app/src/components/admin/dashboard.jsx
+++ router-app/src/components/admin/dashboard.jsx
@@ -1,10 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
+import { Route } from "react-router-dom";
 
-class Dashboard extends Component {
-  state = {};
-  render() {
-    return <h1>Dashboard</h1>;
-  }
-}
+import SideBar from "./sidebar";
+import Users from "./users";
+import Posts from "./posts";
+
+const Dashboard = ({ match }) => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <SideBar />
+      <Route path="/admin/users" component={Users} />
+      <Route path="/admin/posts" component={Posts} />
+    </div>
+  );
+};
 
 export default Dashboard;
---
+++ router-app/src/components/admin/posts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Posts = () => {
+  return <h1>Posts list</h1>;
+};
+
+export default Posts;
