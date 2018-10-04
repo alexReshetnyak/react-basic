f40e4f42285ee4eab84573d1d707dd01a3ea846b
AReshetnyak <areshetnyak@qualium-systems.com.ua>
2018-10-04 18:22:44 +0300

    added sentry errors handler

--- http-app/src/App.js
+++ http-app/src/App.js
@@ -1,71 +1,123 @@
 import React, { Component } from "react";
-import axios from "axios";
+import * as Sentry from "@sentry/browser";
+import { ToastContainer } from "react-toastify";
+import http from "./services/httpService";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import config from "./config.json";
 
 class App extends Component {
   state = {
-    posts: []
+    posts: [],
+    error: null
   };
 
+  componentDidCatch(error, errorInfo) {
+    console.log("Errors", error, errorInfo);
+
+    this.setState({ error });
+    Sentry.configureScope(scope => {
+      Object.keys(errorInfo).forEach(key => {
+        scope.setExtra(key, errorInfo[key]);
+      });
+    });
+    Sentry.captureException(error);
+  }
+
   async componentDidMount() {
-    const { data: posts } = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
+    const { data: posts } = await http.get(config.apiEndpoint);
     this.setState({ posts });
   }
 
-  handleAdd = () => {
-    console.log("Add");
+  handleAdd = async () => {
+    const obj = { title: "a", body: "b" };
+    const { data: post } = await http.post(config.apiEndpoint, obj);
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
   };
 
-  handleUpdate = post => {
-    console.log("Update", post);
+  handleUpdate = async post => {
+    post.title = "UPDATED";
+    await http.put(`${config.apiEndpoint}/${post.id}`, post);
+
+    const posts = [...this.state.posts];
+    const index = posts.indexOf(post);
+    posts[index] = { ...post };
+    this.setState({ posts });
   };
 
-  handleDelete = post => {
-    console.log("Delete", post);
+  handleDelete = async post => {
+    // * Optimistic way
+    const originalPosts = this.state.posts;
+
+    const posts = this.state.posts.filter(p => p.id !== post.id);
+    this.setState({ posts });
+
+    try {
+      await http.delete(`${config.apiEndpoint}/${post.id}`);
+    } catch (error) {
+      // * Handle expected Errors
+      if (error.response && error.response.status === 404) {
+        alert("This post already deleted");
+      }
+
+      this.setState({ posts: originalPosts });
+    }
+  };
+
+  handleError = () => {
+    throw new Error("start error");
   };
 
   render() {
-    return (
-      <React.Fragment>
-        <button className="btn btn-primary" onClick={this.handleAdd}>
-          Add
-        </button>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Update</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.posts.map(post => (
-              <tr key={post.id}>
-                <td>{post.title}</td>
-                <td>
-                  <button
-                    className="btn btn-info btn-sm"
-                    onClick={() => this.handleUpdate(post)}
-                  >
-                    Update
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={() => this.handleDelete(post)}
-                  >
-                    Delete
-                  </button>
-                </td>
+    if (this.state.error) {
+      //render fallback UI
+      return <a onClick={() => Sentry.showReportDialog()}>Report feedback</a>;
+    } else {
+      return (
+        <React.Fragment>
+          <ToastContainer />
+          <button className="btn btn-primary" onClick={this.handleAdd}>
+            Add
+          </button>
+          <button className="btn btn-primary" onClick={this.handleError}>
+            Create Error
+          </button>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Update</th>
+                <th>Delete</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </React.Fragment>
-    );
+            </thead>
+            <tbody>
+              {this.state.posts.map(post => (
+                <tr key={post.id}>
+                  <td>{post.title}</td>
+                  <td>
+                    <button
+                      className="btn btn-info btn-sm"
+                      onClick={() => this.handleUpdate(post)}
+                    >
+                      Update
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger btn-sm"
+                      onClick={() => this.handleDelete(post)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </React.Fragment>
+      );
+    }
   }
 }
 
--- http-app/src/index.js
+++ http-app/src/index.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import * as Sentry from "@sentry/browser";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+
+Sentry.init({
+  dsn: "https://740d2a36f4434e868ce741a40f6def4a@sentry.io/1294631"
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
